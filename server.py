b8e4cd45154629229df61c82b64d50af18a2de0b
Samuel Baraka <barakasamuel125@gmail.com>
2025-09-27 00:15:44 +0300

    Prepare for Render deployment

--- server.py
+++ server.py
@@ -283,4 +283,6 @@ def db_status():
 
 if __name__ == '__main__':
     init_db()
-    app.run(debug=True, port=8000)
+    import os
+    port = int(os.environ.get('PORT', 8000))
+    app.run(host='0.0.0.0', port=port, debug=False)
